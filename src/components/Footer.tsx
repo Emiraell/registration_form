1b72cab7bcb54a516c5e93142b1350cb658b7718
emirael <emmzex19@gmail.com>
2024-06-26 21:42:17 +0100

    link tag to facebook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <div className="mt-8 bg-blue-600 text-center py-2 italic">
-      &#169; Ayf Lafia Diocese copyright 2024 | All right reserved
+      &#169; Ayf Lafia Diocese copyright 2024 | All right reserved <br />
+      Designed and mainted by{" "}
+      <span className="text-blue-100 underline cursor-pointer">
+        <a
+          href="https://www.facebook.com/profile.php?id=100089929903770&mibextid=LQQJ4d"
+          target="_blank"
+        >
+          {" "}
+          Keffi archdeaconry
+        </a>
+      </span>
     </div>
   );
 }
